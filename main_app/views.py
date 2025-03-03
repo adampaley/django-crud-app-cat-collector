7bf821b4aebd76389b2ba8745f4b0bed351128cb
Adam Paley <apaley95@gmail.com>
2025-03-02 23:51:04 -0500

    completed Django One-to-Many Relationships lesson

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,8 @@
 # imports
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Cat 
+from .forms import FeedingForm
 
 # view functions
 def home(request):
@@ -42,4 +43,16 @@ def cat_index(request):
 
 def cat_detail(request, cat_id):
     cat = Cat.objects.get(id=cat_id)
-    return render(request, 'cats/detail.html', {'cat': cat})
+    feeding_form = FeedingForm()
+    return render(request, 'cats/detail.html', {
+        'cat': cat,
+        'feeding_form': feeding_form,
+    })
+
+def add_feeding(request, cat_id):
+    form = FeedingForm(request.POST)
+    if form.is_valid():
+        new_feeding = form.save(commit=False)
+        new_feeding.cat_id = cat_id 
+        new_feeding.save() 
+    return redirect('cat-detail', cat_id=cat_id)
