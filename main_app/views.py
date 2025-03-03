ce9f277b5c49e474d8138e03d99b132be3ee79e4
Adam Paley <apaley95@gmail.com>
2025-03-03 17:56:32 -0500

    completed Django Many-to-Many Relationships lesson

--- main_app/views.py
+++ main_app/views.py
@@ -28,7 +28,7 @@ def about(request):
 
 class CatCreate(CreateView):
     model = Cat
-    fields = '__all__'
+    fields = ['name', 'breed', 'description', 'age']
 
 class CatUpdate(UpdateView):
     model = Cat 
@@ -62,10 +62,12 @@ def cat_index(request):
 
 def cat_detail(request, cat_id):
     cat = Cat.objects.get(id=cat_id)
+    toys_cat_doesnt_have = Toy.objects.exclude(id__in = cat.toys.all().values_list('id'))
     feeding_form = FeedingForm()
     return render(request, 'cats/detail.html', {
         'cat': cat,
         'feeding_form': feeding_form,
+        'toys': toys_cat_doesnt_have
     })
 
 def add_feeding(request, cat_id):
@@ -75,3 +77,13 @@ def add_feeding(request, cat_id):
         new_feeding.cat_id = cat_id 
         new_feeding.save() 
     return redirect('cat-detail', cat_id=cat_id)
+
+def associate_toy(request, cat_id, toy_id):
+    Cat.objects.get(id=cat_id).toys.add(toy_id)
+    return redirect('cat-detail', cat_id=cat_id)
+
+def remove_toy(reqest, cat_id, toy_id):
+    cat = Cat.objects.get(id=cat_id)
+    toy = Toy.objects.get(id=toy_id)
+    cat.toys.remove(toy)
+    return redirect('cat-detail', cat_id=cat.id)
